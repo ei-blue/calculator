0dd80c6116e5b5249cc457c02181b97e00c32aec
Ah-oy <58558581+ei-blue@users.noreply.github.com>
2022-03-07 15:13:37 -0800

    Adding app.py

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# calculator
+# Calculator
+This is an assignment for Back-End Web Architecture class to build a simple API that allows an HTML based calculator to compute subtraction, addition and multiplication of two numbers.
+
+## Endpoints
+1) A GET endpoint "/add" that accepts two args as part of URL parameters (num1 and num2) and returns num1 + num2
+
+2) A POST endpoint "/sub" that accepts two args as part of x-www-form-urlencoded content type (num1 and num2) and returns num1 - num2
+
+3) A POST endpoint "/mult" that accepts two args as part of application/json content type (num1 and num2) and returns num1 * num2
---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/add', methods=["GET"])
+def add():
+    num1 = request.args.get("num1", type=int)  
+    num2 = request.args.get("num2", type=int)
+    return { "answer": num1 + num2 }
+
+@app.route('/sub', methods=["POST"])
+def sub():
+    num1 = request.form.get("num1", type=int)  
+    num2 = request.form.get("num2", type=int)
+    return { "answer": num1 - num2 }
+
+@app.route('/mult', methods=["POST"])
+def mult():
+    num1 = int(request.get_json().get("num1")) 
+    num2 = int(request.get_json().get("num2"))
+    return { "answer": num1 * num2 }
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+app.run(port=8000)
+
+
+
